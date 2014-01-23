dabd1881b203209f7d2e2ef91effef108d585ef8
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-01-23 13:16:09 +0000

    Added verification for field length validation

--- support/step_definitions/01_Start_a_case/01_Personal_details_steps.rb
+++ support/step_definitions/01_Start_a_case/01_Personal_details_steps.rb
@@ -9,3 +9,15 @@ end
 Then(/^there to be an (.*) message$/) do |message|
   within(:css, ".error-summary") { page.should have_content message }
 end
+
+When(/^I am filling in (.*) with (\d+)$/) do |item, num|
+  @app.repossession_claim.step_1.tenants.tenant do
+    |tenant| tenant.send(item.to_sym).set ("x" * "num".to_i)
+  end
+end
+
+Then(/^I expect to get a (\w+) with (.*)$/) do |status, message|
+  if status == "fail"
+    within(:css, ".error-summary") { page.should have_content message }
+  end
+end
